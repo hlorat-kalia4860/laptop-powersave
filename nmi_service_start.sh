b652e1d2346abd7c4a0916b16ef797a031b05521
hlorat-kalia4860 <132037492+hlorat-kalia4860@users.noreply.github.com>
2023-05-15 15:55:07 +0500

    Update nmi_service_start.sh

--- nmi_service_start.sh
+++ nmi_service_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if grep "0" /sys/class/power_supply/ADP1/online
 then 
-source /usr/bin/nmi_watchdog_service/nmi_off.sh
+source /usr/bin/nmi_off.sh
 else
-source /usr/bin/nmi_watchdog_service/nmi_on.sh
+source /usr/bin/nmi_on.sh
 fi
