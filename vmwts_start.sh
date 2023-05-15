dafb3f36437e7c5eff8734837086b1d905721ce2
hlorat-kalia4860 <132037492+hlorat-kalia4860@users.noreply.github.com>
2023-05-15 15:55:22 +0500

    Update vmwts_start.sh

--- vmwts_start.sh
+++ vmwts_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if grep "0" /sys/class/power_supply/ADP1/online
 then 
-source /usr/bin/VMWTS/time_up.sh
+source /usr/bin/time_up.sh
 else
-source /usr/bin/VMWTS/time_down.sh
+source /usr/bin/time_down.sh
 fi
